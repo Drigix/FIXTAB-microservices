21647048f4678fcb0bde14a0c300bb211cf6fb28
drigix <michrarr@gmail.com>
2023-03-22 12:37:47 +0100

    add docker files and configure client application

--- client-application/src/app/app.module.ts
+++ client-application/src/app/app.module.ts
@@ -4,17 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ButtonModule } from 'primeng/button';
-import { InputTextModule } from 'primeng/inputtext';
+import {InputTextModule} from 'primeng/inputtext';
+import { LoginPageComponent } from './auth/login-page/login-page.component';
+import { ComponentsModule } from './components/components.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ButtonModule,
-    InputTextModule
+    InputTextModule,
+    ComponentsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
